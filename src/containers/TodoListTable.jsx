a74d9276d2f09cf8ce61748241d3e5b45a3f09eb
William Parlow Nichelatti <williamn@weg.net>
2019-06-18 10:42:07 -0300

    Created connection and CRUD with indexDB

--- src/containers/TodoListTable.jsx
+++ src/containers/TodoListTable.jsx
@@ -3,6 +3,8 @@ import Table from '../components/Table/Table';
 import './TodoListTable.css';
 import Form from '../components/Form/Form';
 import Footer from '../components/Footer/Footer';
+import IndexDBService from '../services/IndexDBService';
+import { isTemplateElement } from '@babel/types';
 
 export class TodoListTable extends Component {
 
@@ -23,13 +25,44 @@ export class TodoListTable extends Component {
             }
         ];
         this.headitems = ['Task', 'Status'];
+        
         this.onSubmit = this.onSubmit.bind(this);
         this.onChange = this.onChange.bind(this);
+        this.onButtonClearDatabaseClick = this.onButtonClearDatabaseClick.bind(this);
+
+        this._dbService = new IndexDBService('todo-list', 1);
+        this._dbService.createNewStore('todo-iten', {
+            keyPath: 'id', 
+            autoIncrement: true
+        }, [{
+                name: 'task',
+                keyPath: 'task',
+                config: { unique: false }
+            },
+            {
+                name: 'status',
+                keyPath: 'status',
+                config: { unique: false }
+            }
+        ]).then(res => this._dbService.getAllFromStore('todo-iten')
+            .then(data => this.setState({...this.state, todoItens: this.state.todoItens.concat(data.map(iten => {
+                delete iten.id; return iten}
+                ))}))
+            .catch(error => console.log(error)));
     }
 
     onSubmit(event) {
         event.preventDefault();
-        console.log(this.state.task)
+
+        let status = 'Backlog';
+        let task = event.target.task.value;
+
+        this._dbService.addNewFieldValue('todo-iten', {
+            task: task,
+            status: status
+        });
+
+        this.setState({...this.state, todoItens: this.state.todoItens.concat({task: task, status: status})})
     }
 
     onChange(event) {
@@ -37,11 +70,19 @@ export class TodoListTable extends Component {
         this.inputs.filter(i => i.id === event.target.id)[0].value = event.target.value;
     }
 
+    onButtonClearDatabaseClick() {
+        this._dbService.clearStore('todo-iten');
+        this.setState({...this.state, todoItens: []})
+    }
+
     render() {
         return (
             <div>
                 <div className="form-content-wrapper">
                     <Form formId='form-test' inputs={this.inputs} onSubmit={this.onSubmit} onChange={this.onChange} />
+                    <div className="clear-button-wrapper">
+                        <button onClick={this.onButtonClearDatabaseClick}>Clear</button>
+                    </div>
                 </div>
                 <div className="table-content-wrapper" >
                     <Table caption="Todo itens" headitems={this.headitems} bodyitems={this.state.todoItens} />
